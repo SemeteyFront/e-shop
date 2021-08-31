55d74b0f8d10776d1f4fb0c4d1303a351ab0bbf0
Semetey Boronbaev <boronbaevsemetey@gmail.com>
2021-08-31 16:46:11 +0600

    add modal and add photos

---
+++ src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="banner">
+        <h1>
+          Fashion <br />
+          sale
+        </h1>
+        <button>Check</button>
+      </div>
+  )
+}
+
+export default Home
